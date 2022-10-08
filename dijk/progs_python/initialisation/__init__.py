f7a85b5bc8913eeefd3082b21c30ebfaa76611ab
cyril charignon <cyril.charignon@net-c.com>
2022-10-08 14:36:07 +0200

    (gros) fichier des communes de France

--- dijk/progs_python/initialisation/__init__.py
+++ dijk/progs_python/initialisation/__init__.py
@@ -4,5 +4,5 @@ Fonctions permettant de remplir la base.
 Ce module ne sera pas chargé lors d’une utilisation de routine de l’appli. Il utilise la bibliotèque osmnx qui est longue à charger.
 """
 
-from .initialisation import charge_ville
+from .initialisation import charge_ville, charge_villes
 from .initialisation import charge_zone
