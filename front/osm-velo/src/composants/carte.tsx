5d3ffe10eb587fa4bfaee63f6e7e29b41d7d4da5
cyril charignon <cyril.charignon@net-c.com>
2023-10-09 15:28:02 +0200

    (front) composant pour la carte

--- front/osm-velo/src/composants/carte.tsx
+++ front/osm-velo/src/composants/carte.tsx
@@ -1,16 +1,31 @@
 import L from "leaflet"
+import { useEffect} from "react";
+import carteIci from "../fonctions/pour_leaflet";
 
 type propsCarte = {
-    carte: L.Map;
-    marqueurs: L.LayerGroup
+    carte: L.Map|null;
+    marqueurs: L.LayerGroup;
+    setCarte: React.Dispatch<React.SetStateAction<L.Map | null>>;
 }
 
 
-export default function Carte({carte, marqueurs}:propsCarte){
+export default function Carte({ carte, setCarte, marqueurs }: propsCarte) {
 
-    
-    
-    return(
+    useEffect(
+	() => {
+	    if (!carte) {
+		const laCarte=carteIci();
+		marqueurs.addTo(laCarte);
+		setCarte(laCarte);
+	    }else{
+		marqueurs.addTo(carte);
+	    }
+            },
+            []
+        )
+
+
+    return (
         <div id="laCarte">
         </div>
     )
--- front/osm-velo/src/pages/autourDeMoi.tsx
+++ front/osm-velo/src/pages/autourDeMoi.tsx
@@ -7,6 +7,9 @@ import FormAutourDeMoi from "../composants/forms/autourDeMoi.tsx";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Carte from "../composants/carte.tsx";
+
+
 
 // Page « Autour de moi »
 // Une carte et un formulaire pour chercher des lieux.
@@ -17,32 +20,20 @@ export default function AutourDeMoi() {
     let [carte, setCarte] = useState<L.Map | null>(null);
     const marqueurs = new L.LayerGroup();
 
-	useEffect(
-		() => {
-		    if (!carte) {
-			const laCarte=carteIci();
-			marqueurs.addTo(laCarte);
-			setCarte(laCarte);
-		    }else{
-			marqueurs.addTo(carte);
-			}
-		},
-		[]
-	)
-
-    
 
     return (
 	<Base>
             <Container>
                 <Row>
+                    
                     <Col md={9} >
-	                <div id="laCarte">
-                        </div>
+                    <Carte carte={carte} setCarte={setCarte} marqueurs={marqueurs} />
 	            </Col>
+                    
                     <Col>
 	                {carte ? <FormAutourDeMoi carte={carte as L.Map} marqueurs={marqueurs} /> : null}
                     </Col>
+                    
                 </Row>
             </Container>
             
